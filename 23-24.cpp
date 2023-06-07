fd7113dc92c88f04775e49d38e24b98e9a05296f
Ashton <askhatuteshev@gmail.com>
2023-06-08 00:02:48 +0600

    23-24 and 25-26 done

---
+++ 23-24.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+
+using namespace std;
+struct LC { LC() { system("chcp 1251 > nul"); system("color E"); } }_;
+
+/*
+Домашнее задание
+Тема. Перегрузка операторов ++, --
+
+Задание 1
+Создайте класс Circle (окружность).
+Реализуйте через перегруженные операторы:
+1. Проверка на равенство радиусов двух окружностей (операция ==);
+2. Сравнения длин двух окружностей (операция < );
+3. Пропорциональное изменение размеров окружности, путем изменения ее радиуса (операция += и -=);
+4. Увеличение и уменьшение радиуса окружности на 1 (операции ++ и --, в префиксной и постфиксной формах);
+
+*/
+
+class Circle
+{
+private:
+	int C;
+	int R;
+	const double P = 3.14;
+
+public:
+	Circle() : C(), R() {}
+	Circle(int C) { this->C = C; }
+	Circle(int C, int R)
+	{
+		this->C = C;
+		this->R = R;
+	}
+	void SetC(int C) { this->C = C; }
+	void SetR(int R) { this->R = R; }
+
+	int GetC() const { return C; }
+	int GetR() const { return R; }
+
+	friend istream& operator >> (istream& is, Circle obj)
+	{
+		cout << "Input Circumference of Circle: ";
+		is >> obj.C;
+		cout << "Input Radius of Circle: ";
+		is >> obj.R;
+		return is;
+	}
+	friend ostream& operator << (ostream& os, Circle obj)
+	{
+		os << "Circumference of Circke is: " << obj.C << endl;
+		os << "Radius of Circke is: " << obj.R << endl;
+		return os;
+	}
+
+	bool operator == (Circle circum)
+	{
+		return this->C == circum.C;
+	}
+
+	bool operator < (Circle circum)
+	{
+		return this->C < circum.C;
+	}
+
+	Circle operator += (int R)
+	{
+		this->R = R;
+		return C = (2 * P * R);
+	}
+
+	Circle operator ++()
+	{
+		++R;
+		return *this;
+	}
+
+	Circle operator ++(int)
+	{
+		R++;
+		return *this;
+	}
+
+
+}; //Circle
+
+	void CheckC(Circle first, Circle second)
+	{
+		if (first < second)
+			cout << "First Circle < than second;" << endl;
+		else
+			cout << "First Circle > than second;" << endl;
+
+	}
+
+
+
+int main()
+{
+	Circle A;
+	Circle B;
+	A.SetC(25);
+	B.SetC(10);
+	CheckC(A,B);
+	A += 20;
+	cout << A;
+	A++;
+	cout << A;
+	++A;
+	cout << A;
+}
---
+++ 25-26.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#define RAND(Min,Max) (rand()%((Max)-(Min)+1)+(Min))
+using namespace std;
+struct LC { LC() { system("chcp 1251 > nul"); system("color E"); } }_;
+/*
+Домашнее задание
+Шаблоны функций, классов
+
+
+Задание 1
+Написать перегруженные шаблоны функций для нахождения корней линейного (a * x + b = 0) и квадратного (a * x**2 + b * x + c = 0) уравнений.
+Замечание: в функции передаются коэффициенты уравнений.
+
+
+Задание 2
+Напишите шаблон функции, которая возвращает максимум из двух переданных параметров.
+
+
+Задание 3
+Напишите шаблон функции, которая находит минимальное значение в массиве и возвращает одновременно найденное минимальное значение и его позицию в массиве.
+
+*/
+
+template <class T>
+T MaxReturn(T a, T b)
+{
+	T Result = 0;
+	if (a > b)
+		cout << "MAX is: " << a << endl;
+	else
+		cout << "MAX is: " << b << endl;
+	return Result;
+
+}
+
+template <class T2>
+T2 MinInArr(T2 * arr, T2 size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = RAND(1, 20);
+	}
+
+	int min = arr[0];
+	int Index = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+
+		if (arr[i] < min)
+		{
+			min = arr[i];
+			Index = i;
+		}
+
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl << "Min Value of this array is: " << min << "," << " in " << Index + 1 << " place " << endl;
+	return min;
+}
+
+
+int main()
+{
+	MaxReturn <int> (15, 50);
+	int arr[] = { 1,2,3,4,5};
+	MinInArr(arr, 5);
+}
